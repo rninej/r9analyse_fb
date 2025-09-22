830065034b325831c011edfbc086e5e0f3456345
R9SME <102382866+Arnavjain1009@users.noreply.github.com>
2025-09-22 20:53:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/r9analyse_fb',
+  assetPrefix: '/r9analyse_fb/',
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, { isServer }) => {
@@ -11,3 +14,17 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+/**
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+};
+module.exports = nextConfig;
+*/
