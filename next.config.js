0ce746a4daf352b6280ab258c32922c2fd0778b6
techhotspot.store <techhotspot.store@gmail.com>
2025-09-23 20:28:42 +0000

    my upd

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/r9analyse_fb',
-  assetPrefix: '/r9analyse_fb/',
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, { isServer }) => {
@@ -14,17 +11,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-/**
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
-};
-module.exports = nextConfig;
-*/
