6c55bbd17858ab2745eae42495b045bc70426cc1
arnavjain1009 <arnavjain1009@gmail.com>
2025-10-27 19:53:00 +0000

    fix: add android platform

---
+++ android/app/src/main/java/com/mranalyse/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mranalyse.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ out/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+  console.log('Service worker installed');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activated');
+});
+
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+  });
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/')
+  );
+});
